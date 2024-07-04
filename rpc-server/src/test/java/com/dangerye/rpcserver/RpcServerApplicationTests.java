f4c094223488109258158e2a42f211fe26697e95
dangerye <dangerye@me.com>
2024-07-04 21:54:37 +0800

    rpc init

---
+++ rpc-server/src/test/java/com/dangerye/rpcserver/RpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangerye.rpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
