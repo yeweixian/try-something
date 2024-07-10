518dd3b9b39c7562b0e40809857320ccbe9215bd
dangerye <dangerye@me.com>
2024-07-11 00:33:23 +0800

    wechat code v1

---
+++ my-wechat-server/src/main/java/com/dangerye/mywechatserver/MyWechatServerApplication.java
@@ -0,0 +1,13 @@
+package com.dangerye.mywechatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWechatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWechatServerApplication.class, args);
+    }
+
+}
---
+++ my-wechat-server/src/test/java/com/dangerye/mywechatserver/MyWechatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangerye.mywechatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWechatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
