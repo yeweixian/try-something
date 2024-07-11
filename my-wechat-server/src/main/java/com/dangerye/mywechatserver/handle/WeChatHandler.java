b47a24f3045899923319dad06608dfbfb425fc03
dangerye <dangerye@me.com>
2024-07-11 10:27:42 +0800

    wechat code v4

--- my-wechat-server/src/main/java/com/dangerye/mywechatserver/handle/WeChatHandler.java
+++ my-wechat-server/src/main/java/com/dangerye/mywechatserver/handle/WeChatHandler.java
@@ -7,12 +7,45 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 @ChannelHandler.Sharable
 public class WeChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    public final List<Channel> channelList = new ArrayList<>();
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        final Channel channel = ctx.channel();
+        channelList.add(channel);
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        final Channel channel = ctx.channel();
+        channelList.remove(channel);
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        final Channel channel = ctx.channel();
+        channelList.remove(channel);
+        super.exceptionCaught(ctx, cause);
+    }
+
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
         final String msg = textWebSocketFrame.text();
         final Channel channel = channelHandlerContext.channel();
+        for (Channel item : channelList) {
+            if (channel != item) {
+                item.writeAndFlush(new TextWebSocketFrame(msg));
+            }
+        }
     }
 }
