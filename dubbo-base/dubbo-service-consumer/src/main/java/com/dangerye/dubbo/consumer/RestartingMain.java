093d2cc9b3dffac9228bdeab3fd6eac0b816c53b
dangerye <dangerye@me.com>
2024-07-30 02:50:34 +0800

    try spi v25

--- dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/RestartingMain.java
+++ dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/RestartingMain.java
@@ -1,10 +1,11 @@
 package com.dangerye.dubbo.consumer;
 
+import com.dangerye.base.utils.SingletonLoader;
 import com.dangerye.router.ReadyRestartInstances;
 
 public class RestartingMain {
     public static void main(String[] args) throws Exception {
-        final ReadyRestartInstances instance = ReadyRestartInstances.getInstance();
+        final ReadyRestartInstances instance = SingletonLoader.getInstance(ReadyRestartInstances.class);
         System.out.println(instance);
     }
 }
