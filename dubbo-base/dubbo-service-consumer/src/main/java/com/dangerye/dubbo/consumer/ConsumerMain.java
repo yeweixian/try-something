bc73331246f7f3b2fa1621e5d60b297f589efd6c
dangerye <dangerye@me.com>
2024-08-01 18:44:20 +0800

    try spi v32

--- dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/ConsumerMain.java
+++ dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/ConsumerMain.java
@@ -1,6 +1,7 @@
 package com.dangerye.dubbo.consumer;
 
 import com.dangerye.dubbo.consumer.bean.ConsumerComponent;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,13 +9,58 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ConsumerMain {
+
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
     public static void main(String[] args) throws IOException {
         final AnnotationConfigApplicationContext context
                 = new AnnotationConfigApplicationContext(ConsumerConfiguration.class);
         context.start();
         final ConsumerComponent consumerComponent = context.getBean(ConsumerComponent.class);
+        new Thread(() -> {
+            while (true) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(RandomUtils.nextLong(5, 15));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                EXECUTOR_SERVICE.execute(() -> {
+                    final String s = consumerComponent.methodA();
+                    System.out.println(s);
+                });
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(RandomUtils.nextLong(5, 15));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                EXECUTOR_SERVICE.execute(() -> {
+                    final String s = consumerComponent.methodB();
+                    System.out.println(s);
+                });
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(RandomUtils.nextLong(5, 15));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                EXECUTOR_SERVICE.execute(() -> {
+                    final String s = consumerComponent.methodC();
+                    System.out.println(s);
+                });
+            }
+        }).start();
         while (true) {
             System.in.read();
             final String result = consumerComponent.sayHello("world.");
--- dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/bean/ConsumerComponent.java
+++ dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/bean/ConsumerComponent.java
@@ -1,5 +1,6 @@
 package com.dangerye.dubbo.consumer.bean;
 
+import com.dangerye.dubbo.service.api.BaseService;
 import com.dangerye.dubbo.service.api.HelloService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,22 @@ import org.springframework.stereotype.Component;
 public class ConsumerComponent {
     @Reference(loadbalance = "onlyFirst")
     private HelloService helloService;
+    @Reference(loadbalance = "onlyFirst")
+    private BaseService baseService;
 
     public String sayHello(String name) {
         return helloService.sayHello(name);
+    }
+
+    public String methodA() {
+        return baseService.methodA();
+    }
+
+    public String methodB() {
+        return baseService.methodB();
+    }
+
+    public String methodC() {
+        return baseService.methodC();
     }
 }
