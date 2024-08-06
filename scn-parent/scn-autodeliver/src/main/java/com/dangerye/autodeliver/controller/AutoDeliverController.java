0078c55b61c567763dde0b4dfe21ac78aaea0f10
dangerye <dangerye@me.com>
2024-08-06 16:44:33 +0800

    scn code v7

--- scn-parent/scn-autodeliver/src/main/java/com/dangerye/autodeliver/controller/AutoDeliverController.java
+++ scn-parent/scn-autodeliver/src/main/java/com/dangerye/autodeliver/controller/AutoDeliverController.java
@@ -1,10 +1,14 @@
 package com.dangerye.autodeliver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/autoDeliver")
@@ -12,9 +16,22 @@ public class AutoDeliverController {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
+    /*
+        @RequestMapping("/checkStatus/{userId}")
+        public Integer checkStatusByUserId(@PathVariable Long userId) {
+            return restTemplate.getForObject("http://localhost:8081/resume/status/" + userId, Integer.class);
+        }
+    */
     @RequestMapping("/checkStatus/{userId}")
     public Integer checkStatusByUserId(@PathVariable Long userId) {
-        return restTemplate.getForObject("http://localhost:8081/resume/status/" + userId, Integer.class);
+        final List<ServiceInstance> instances = discoveryClient.getInstances("resume-application");
+        final ServiceInstance serviceInstance = instances.get(0);
+        final String host = serviceInstance.getHost();
+        final int port = serviceInstance.getPort();
+        final String url = "http://" + host + ":" + port + "/resume/status/" + userId;
+        return restTemplate.getForObject(url, Integer.class);
     }
 }
