42f10c9f793fe475a2d13aa9eb5857d75d134b0a
dangerye <dangerye@me.com>
2024-08-06 15:54:09 +0800

    scn code v5

---
+++ scn-parent/scn-eureka-service-8762/src/main/java/com/dangerye/eureka/EurekaServerApplication8762.java
@@ -0,0 +1,13 @@
+package com.dangerye.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication8762 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication8762.class, args);
+    }
+}
