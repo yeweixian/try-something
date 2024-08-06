c5197b12ddd5c8856f73665e35c3651b7301e06b
dangerye <dangerye@me.com>
2024-08-06 17:04:10 +0800

    scn code v9

--- my-wechat-server/src/test/java/com/dangerye/mywechatserver/MyWechatServerApplicationTests.java
+++ my-wechat-server/src/test/java/com/dangerye/mywechatserver/MyWechatServerApplicationTests.java
@@ -1,9 +1,12 @@
 package com.dangerye.mywechatserver;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 class MyWechatServerApplicationTests {
 
     @Test
