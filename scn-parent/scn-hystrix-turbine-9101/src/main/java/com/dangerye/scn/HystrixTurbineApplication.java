1f5f140e30b8bffccb8c4e751eb0072a81638fbd
dangerye <dangerye@me.com>
2024-08-09 16:51:03 +0800

    scn code v16

---
+++ scn-parent/scn-hystrix-turbine-9101/src/main/java/com/dangerye/scn/HystrixTurbineApplication.java
@@ -0,0 +1,15 @@
+package com.dangerye.scn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
+public class HystrixTurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixTurbineApplication.class, args);
+    }
+}
