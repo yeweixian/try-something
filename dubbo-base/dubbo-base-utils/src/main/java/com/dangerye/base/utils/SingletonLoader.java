29ed9d78b271f9c1ae0628c70345c1ce7c118b69
dangerye <dangerye@me.com>
2024-08-16 11:32:39 +0800

    scn code v53

--- dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/SingletonLoader.java
+++ dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/SingletonLoader.java
@@ -1,13 +1,17 @@
 package com.dangerye.base.utils;
 
+import org.apache.dubbo.common.extension.ExtensionLoader;
+
 import java.util.concurrent.ConcurrentHashMap;
 
-public final class SingletonLoader<C> {
+public final class SingletonLoader<I> {
     private static final ConcurrentHashMap<Class<?>, SingletonLoader<?>> SINGLETON_LOADER_MAP = new ConcurrentHashMap<>();
-    private final Class<C> clazz;
+    private final Class<I> clazz;
+    private final ExtensionLoader<SingletonInstance> singletonInstanceExtensionLoader;
 
-    private SingletonLoader(Class<C> clazz) {
+    private SingletonLoader(Class<I> clazz) {
         this.clazz = clazz;
+        this.singletonInstanceExtensionLoader = ExtensionLoader.getExtensionLoader(SingletonInstance.class);
     }
 
     @SuppressWarnings("unchecked")
