4d69c70a55628d3884e9dee5129d34a2236dc7cd
dangerye <dangerye@me.com>
2024-08-16 15:34:48 +0800

    scn code v57

--- dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/impl/DubboRouterZkClient.java
+++ dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/impl/DubboRouterZkClient.java
@@ -1,14 +1,22 @@
 package com.dangerye.base.utils.impl;
 
-import com.dangerye.base.utils.SingletonLoader;
-import com.dangerye.base.utils.ZookeeperClientInstance;
-import com.dangerye.base.utils.ZookeeperUtil;
+import com.dangerye.base.utils.SingletonInstance;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 
-public class DubboRouterZkClient implements ZookeeperClientInstance {
+public class DubboRouterZkClient implements SingletonInstance<CuratorFramework> {
     @Override
-    public CuratorFramework getClient() {
-        final ZookeeperUtil zookeeperUtil = SingletonLoader.getInstance(ZookeeperUtil.class);
-        return zookeeperUtil.getDubboRouterZkClient();
+    public CuratorFramework getInstance() {
+        final ExponentialBackoffRetry retry = new ExponentialBackoffRetry(1000, 3);
+        final CuratorFramework zkClient = CuratorFrameworkFactory.builder()
+                .connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(5000)
+                .connectionTimeoutMs(3000)
+                .retryPolicy(retry)
+                .namespace("dubboRouter")
+                .build();
+        zkClient.start();
+        return zkClient;
     }
 }
--- dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/impl/MyRPCServicesZkClient.java
+++ dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/impl/MyRPCServicesZkClient.java
@@ -1,14 +1,22 @@
 package com.dangerye.base.utils.impl;
 
-import com.dangerye.base.utils.SingletonLoader;
-import com.dangerye.base.utils.ZookeeperClientInstance;
-import com.dangerye.base.utils.ZookeeperUtil;
+import com.dangerye.base.utils.SingletonInstance;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 
-public class MyRPCServicesZkClient implements ZookeeperClientInstance {
+public class MyRPCServicesZkClient implements SingletonInstance<CuratorFramework> {
     @Override
-    public CuratorFramework getClient() {
-        final ZookeeperUtil zookeeperUtil = SingletonLoader.getInstance(ZookeeperUtil.class);
-        return zookeeperUtil.getMyRPCServicesZkClient();
+    public CuratorFramework getInstance() {
+        final ExponentialBackoffRetry retry = new ExponentialBackoffRetry(1000, 3);
+        final CuratorFramework curatorFramework = CuratorFrameworkFactory.builder()
+                .connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(5000)
+                .connectionTimeoutMs(3000)
+                .retryPolicy(retry)
+                .namespace("myRPCServices")
+                .build();
+        curatorFramework.start();
+        return curatorFramework;
     }
 }
