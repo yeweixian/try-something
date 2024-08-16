7d8a713be8989bb4c2cc10a59a5b834dc5958c6b
dangerye <dangerye@me.com>
2024-08-16 12:06:15 +0800

    scn code v54

--- dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/Loader.java
+++ dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/Loader.java
@@ -1,15 +1,9 @@
 package com.dangerye.base.utils;
 
-import java.lang.reflect.Method;
 import java.util.function.Supplier;
 
 public final class Loader<C> {
-    private final Class<C> clazz;
     private volatile C instance;
-
-    public Loader(Class<C> clazz) {
-        this.clazz = clazz;
-    }
 
     public C getInstance(Supplier<C> supplier) {
         if (instance == null) {
@@ -22,21 +16,6 @@ public final class Loader<C> {
             }
         } else {
             return instance;
-        }
-    }
-
-    public C getInstance() {
-        return getInstance(this::createInstance);
-    }
-
-    @SuppressWarnings("unchecked")
-    private C createInstance() {
-        try {
-            final Method staticMethod = clazz.getDeclaredMethod("getInstance");
-            staticMethod.setAccessible(true);
-            return (C) staticMethod.invoke(null);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 }
