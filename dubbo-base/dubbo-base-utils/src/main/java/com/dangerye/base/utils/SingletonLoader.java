7dd22596a4e2c5f864639149daf14e3fafe78371
dangerye <dangerye@me.com>
2024-08-16 14:12:18 +0800

    scn code v55

--- dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/SingletonLoader.java
+++ dubbo-base/dubbo-base-utils/src/main/java/com/dangerye/base/utils/SingletonLoader.java
@@ -1,17 +1,29 @@
 package com.dangerye.base.utils;
 
 import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.apache.dubbo.common.utils.CollectionUtils;
 
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class SingletonLoader<I> {
     private static final ConcurrentHashMap<Class<?>, SingletonLoader<?>> SINGLETON_LOADER_MAP = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, SingletonInstance<?>> SINGLETON_INSTANCE_MAP = new ConcurrentHashMap<>();
+
+    static {
+        final ExtensionLoader<SingletonInstance> extensionLoader = ExtensionLoader.getExtensionLoader(SingletonInstance.class);
+        final Set<String> supportedExtensions = extensionLoader.getSupportedExtensions();
+        if (CollectionUtils.isNotEmpty(supportedExtensions)) {
+            for (String name : supportedExtensions) {
+                SINGLETON_INSTANCE_MAP.computeIfAbsent(name, extensionLoader::getExtension);
+            }
+        }
+    }
+
     private final Class<I> clazz;
-    private final ExtensionLoader<SingletonInstance> singletonInstanceExtensionLoader;
 
     private SingletonLoader(Class<I> clazz) {
         this.clazz = clazz;
-        this.singletonInstanceExtensionLoader = ExtensionLoader.getExtensionLoader(SingletonInstance.class);
     }
 
     @SuppressWarnings("unchecked")
