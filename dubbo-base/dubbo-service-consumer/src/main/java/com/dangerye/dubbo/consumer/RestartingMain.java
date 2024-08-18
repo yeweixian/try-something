f2d040411754c9a921d8ce4d4f47fb486b3907b8
dangerye <dangerye@me.com>
2024-08-18 15:40:42 +0800

    scn code v71

--- dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/RestartingMain.java
+++ dubbo-base/dubbo-service-consumer/src/main/java/com/dangerye/dubbo/consumer/RestartingMain.java
@@ -5,8 +5,13 @@ import com.dangerye.router.utils.RestartServiceRiskControlUtil;
 
 public class RestartingMain {
     public static void main(String[] args) throws Exception {
-        final SingletonLoader<RestartServiceRiskControlUtil> singletonLoader = SingletonLoader.getSingletonLoader(RestartServiceRiskControlUtil.class);
-        final RestartServiceRiskControlUtil riskControlUtil = singletonLoader.getSingletonInstance("restartServiceRiskControlUtil");
-        System.out.println(riskControlUtil);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                final SingletonLoader<RestartServiceRiskControlUtil> singletonLoader = SingletonLoader.getSingletonLoader(RestartServiceRiskControlUtil.class);
+                System.out.println(singletonLoader);
+                final RestartServiceRiskControlUtil riskControlUtil = singletonLoader.getSingletonInstance("restartServiceRiskControlUtil");
+                System.out.println(riskControlUtil);
+            }).start();
+        }
     }
 }
