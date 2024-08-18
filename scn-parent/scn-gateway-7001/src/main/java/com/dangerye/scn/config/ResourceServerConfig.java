c97b9d8026a53fcbe3a5d71c8f7acf33b075242f
dangerye <dangerye@me.com>
2024-08-19 00:37:59 +0800

    scn code v83

--- scn-parent/scn-gateway-7001/src/main/java/com/dangerye/scn/config/ResourceServerConfig.java
+++ scn-parent/scn-gateway-7001/src/main/java/com/dangerye/scn/config/ResourceServerConfig.java
@@ -2,7 +2,7 @@ package com.dangerye.scn.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
 @EnableResourceServer
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
