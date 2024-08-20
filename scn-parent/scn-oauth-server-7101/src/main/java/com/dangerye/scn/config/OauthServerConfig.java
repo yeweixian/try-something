cd2fbdf342a07c2d3ac5cf9e01837f9798dea0cd
dangerye <dangerye@me.com>
2024-08-20 16:15:36 +0800

    scn code v89

--- scn-parent/scn-oauth-server-7101/src/main/java/com/dangerye/scn/config/OauthServerConfig.java
+++ scn-parent/scn-oauth-server-7101/src/main/java/com/dangerye/scn/config/OauthServerConfig.java
@@ -2,6 +2,7 @@ package com.dangerye.scn.config;
 
 import com.dangerye.scn.utils.Loader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,11 +12,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration      // 当前为 oauth2 server 配置类
 @EnableAuthorizationServer      // 开启认证服务器功能
@@ -23,8 +27,11 @@ public class OauthServerConfig extends AuthorizationServerConfigurerAdapter {
     private final Loader<TokenStore> tokenStoreLoader = new Loader<>();
     private final Loader<JwtAccessTokenConverter> jwtAccessTokenConverterLoader = new Loader<>();
     private final Loader<AuthorizationServerTokenServices> tokenServicesLoader = new Loader<>();
+    private final Loader<JdbcClientDetailsService> jdbcClientDetailsServiceLoader = new Loader<>();
     @Autowired
     private AuthenticationManager authenticationManager;
+    // @Autowired
+    // private DataSource dataSource;
 
     /**
      * 认证服务器 最终以api接口方式 对外提供服务 - 需校验合法性 生成令牌 及 校验令牌 等
@@ -56,7 +63,17 @@ public class OauthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .resourceIds("oauth-resource")
                 .authorizedGrantTypes("password", "refresh_token")
                 .scopes("all");
+        // todo 数据库获取以上信息
+        // clients.withClientDetails(jdbcClientDetailsService());
     }
+
+    /**
+     * JdbcClientDetailsService 中包含表及字段信息;
+
+    @Bean
+    public JdbcClientDetailsService jdbcClientDetailsService() {
+        return jdbcClientDetailsServiceLoader.getInstance(() -> new JdbcClientDetailsService(dataSource));
+    }*/
 
     /**
      * 配置 token 令牌相关的 方法
