54dd9fd394cf8f05e9402862e6ecbfba9d9ee989
dangerye <dangerye@me.com>
2024-08-20 15:51:44 +0800

    scn code v88

--- scn-parent/scn-oauth-resource-api/src/main/java/com/dangerye/scn/config/ResourceServerConfig.java
+++ scn-parent/scn-oauth-resource-api/src/main/java/com/dangerye/scn/config/ResourceServerConfig.java
@@ -1,5 +1,6 @@
 package com.dangerye.scn.config;
 
+import com.dangerye.scn.utils.Loader;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -7,17 +8,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer   // 开启资源服务器功能
 @EnableWebSecurity      // 开启web访问安全
 @EnableConfigurationProperties(ResourceConfig.class)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
+    private final Loader<TokenStore> tokenStoreLoader = new Loader<>();
+    private final Loader<JwtAccessTokenConverter> jwtAccessTokenConverterLoader = new Loader<>();
     @Autowired
     private ResourceConfig resourceConfig;
 
@@ -27,16 +32,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         // 定义 token 服务对象 token校验所需对应的服务对象
-        final RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
-        remoteTokenServices.setCheckTokenEndpointUrl("http://localhost:7101/oauth/check_token");
-        remoteTokenServices.setClientId("business_client");
-        remoteTokenServices.setClientSecret("business_secret");
-        resources.tokenServices(remoteTokenServices);
+        // final RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+        // remoteTokenServices.setCheckTokenEndpointUrl("http://localhost:7101/oauth/check_token");
+        // remoteTokenServices.setClientId("business_client");
+        // remoteTokenServices.setClientSecret("business_secret");
+        // resources.tokenServices(remoteTokenServices);
+        resources.tokenStore(tokenStore()).stateless(true);
         if (StringUtils.isEmpty(resourceConfig.getResourceId())) {
             throw new RuntimeException("ConfigurationProperties: scn.resources.resourceId not find...");
         }
         resources.resourceId(resourceConfig.getResourceId());
         super.configure(resources);
+    }
+
+    private TokenStore tokenStore() {
+        return tokenStoreLoader.getInstance(() -> new JwtTokenStore(jwtAccessTokenConverter()));
+    }
+
+    private JwtAccessTokenConverter jwtAccessTokenConverter() {
+        return jwtAccessTokenConverterLoader.getInstance(() -> {
+            final JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+            jwtAccessTokenConverter.setSigningKey("signingKey");
+            jwtAccessTokenConverter.setVerifier(new MacSigner("signingKey"));
+            return jwtAccessTokenConverter;
+        });
     }
 
     /**
---
+++ scn-parent/scn-oauth-resource-api/src/main/java/com/dangerye/scn/utils/Loader.java
@@ -0,0 +1,21 @@
+package com.dangerye.scn.utils;
+
+import java.util.function.Supplier;
+
+public final class Loader<C> {
+    private volatile C instance;
+
+    public C getInstance(Supplier<C> supplier) {
+        if (instance == null) {
+            synchronized (this) {
+                if (instance == null) {
+                    return instance = supplier.get();
+                } else {
+                    return instance;
+                }
+            }
+        } else {
+            return instance;
+        }
+    }
+}
