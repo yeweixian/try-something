89243f987d999ab1ee1659d706e73c8c5812c35a
dangerye <dangerye@me.com>
2024-08-24 16:29:51 +0800

    sca code v4

--- scn-parent/scn-autodeliver-8092/src/main/java/com/dangerye/autodeliver/AutoDeliverApplication.java
+++ scn-parent/scn-autodeliver-8092/src/main/java/com/dangerye/autodeliver/AutoDeliverApplication.java
@@ -1,6 +1,5 @@
 package com.dangerye.autodeliver;
 
-import com.dangerye.scn.config.EnableResourceServerClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
