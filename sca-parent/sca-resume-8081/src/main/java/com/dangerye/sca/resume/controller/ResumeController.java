5dd10d728c0b08a7455129ac714412f2ed4d0d57
dangerye <dangerye@me.com>
2024-08-26 17:01:45 +0800

    sca code v8

--- sca-parent/sca-resume-8081/src/main/java/com/dangerye/sca/resume/controller/ResumeController.java
+++ sca-parent/sca-resume-8081/src/main/java/com/dangerye/sca/resume/controller/ResumeController.java
@@ -1,6 +1,8 @@
 package com.dangerye.sca.resume.controller;
 
 import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,17 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/resume")
+@RefreshScope
 public class ResumeController {
+
+    @Value("${server.config.test}")
+    private String serverConfigTest;
+
+    @RequestMapping("/showConfigMsg")
+    public String showConfigTest() {
+        return serverConfigTest;
+    }
+
     @RequestMapping("/status/{userId}")
     public Integer getStatusByUserId(@PathVariable Long userId) throws InterruptedException {
         final int i = RandomUtils.nextInt(0, 10);
