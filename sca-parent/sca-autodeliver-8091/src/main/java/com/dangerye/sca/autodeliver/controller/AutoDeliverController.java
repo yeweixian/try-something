570929ab5573e70d4d182e6bf30b8240072065ce
dangerye <dangerye@me.com>
2024-08-27 17:10:26 +0800

    sca code v12

--- sca-parent/sca-autodeliver-8091/src/main/java/com/dangerye/sca/autodeliver/controller/AutoDeliverController.java
+++ sca-parent/sca-autodeliver-8091/src/main/java/com/dangerye/sca/autodeliver/controller/AutoDeliverController.java
@@ -1,5 +1,7 @@
 package com.dangerye.sca.autodeliver.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.dangerye.sca.autodeliver.service.ResumeServiceFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -16,7 +18,12 @@ public class AutoDeliverController {
     private ResumeServiceFeignClient resumeServiceFeignClient;
 
     @RequestMapping("/checkStatus/{userId}")
+    @SentinelResource(value = "checkStatusByUserId", blockHandler = "fallbackCheckStatusByUserId")
     public Integer checkStatusByUserId(@PathVariable Long userId) throws InterruptedException {
         return resumeServiceFeignClient.getStatusByUserId(userId);
+    }
+
+    public Integer fallbackCheckStatusByUserId(Long userId, BlockException blockException) {
+        return -1;
     }
 }
