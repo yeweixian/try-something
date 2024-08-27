0ed312eaab2ac5372bf3aa1b3faf8c90baaaba17
dangerye <dangerye@me.com>
2024-08-28 01:15:25 +0800

    sca code v16

---
+++ sca-parent/sca-dubbo-resume-8084/src/main/java/com/dangerye/sca/resume/ResumeApplication8084.java
@@ -0,0 +1,13 @@
+package com.dangerye.sca.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ResumeApplication8084 {
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeApplication8084.class, args);
+    }
+}
---
+++ sca-parent/sca-dubbo-resume-8084/src/main/java/com/dangerye/sca/resume/service/impl/HelloServiceImpl.java
@@ -0,0 +1,22 @@
+package com.dangerye.sca.resume.service.impl;
+
+import com.dangerye.sca.service.api.HelloService;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.dubbo.config.annotation.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public Integer getStatusByUserId(Long userId) {
+        try {
+            final int i = RandomUtils.nextInt(0, 10);
+            TimeUnit.SECONDS.sleep(i);
+            System.out.println("8084: " + i);
+            return i;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
